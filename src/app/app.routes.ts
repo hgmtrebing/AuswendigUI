c74bedc454f1ca3a0c1ab0cdc26a336d2d696b9d
Harry Geoffrey Trebing <hgmtrebing@gmail.com>
2024-09-02 20:31:53 -0400

    Moved Angular Test Page to its own dedicated component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {AngularComponent} from "./angular/angular.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'angular', component: AngularComponent},
+];
